ca5669fb70066deee2c0229b4a12d8ee811606f1
Vladimir <v1ztep@gmail.com>
2021-04-07 15:32:57 +0300

    переместил сортировку на сторону БД

--- blog/views.py
+++ blog/views.py
@@ -28,14 +28,9 @@ def serialize_tag(tag):
     }
 
 
-def get_likes_count(post):
-    return post.likes__count
-
-
 def index(request):
     posts = Post.objects.annotate(Count('likes'))
-    popular_posts = sorted(posts, key=get_likes_count)
-    most_popular_posts = popular_posts[-5:]
+    most_popular_posts = list(posts.order_by('-likes__count')[:5])
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
