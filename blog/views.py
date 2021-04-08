0caedc4b1f862948c463202e13cdea8e4659cf90
Vladimir <v1ztep@gmail.com>
2021-04-08 20:29:11 +0300

    Уменьшил кол-во запросов к author через prefetch_related

--- blog/views.py
+++ blog/views.py
@@ -29,10 +29,10 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(Count('likes'))
+    posts = Post.objects.annotate(Count('likes')).prefetch_related('author')
     most_popular_posts = list(posts.order_by('-likes__count')[:5])
 
-    fresh_posts = Post.objects.order_by('-published_at')
+    fresh_posts = Post.objects.order_by('-published_at').prefetch_related('author')
     most_fresh_posts = list((fresh_posts)[:5])
 
     tags = Tag.objects.annotate(Count('posts'))
