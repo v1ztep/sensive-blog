55eb0b72368186cd20703d63b3ce557c5476b7cd
Vladimir <v1ztep@gmail.com>
2021-04-09 13:20:10 +0300

    Форкнул serialize_post, добавил annotate+Count на комментарии

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        "title": post.title,
+        "teaser_text": post.text[:200],
+        "author": post.author.username,
+        "comments_amount": post.comments__count,
+        "image_url": post.image.url if post.image else None,
+        "published_at": post.published_at,
+        "slug": post.slug,
+        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -29,18 +43,21 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(Count('likes')).prefetch_related('author')
+    posts = Post.objects.annotate(Count('likes', distinct=True),
+                                  Count('comments', distinct=True))\
+                        .prefetch_related('author')
     most_popular_posts = list(posts.order_by('-likes__count')[:5])
 
-    fresh_posts = Post.objects.order_by('-published_at').prefetch_related('author')
+    fresh_posts = Post.objects.order_by('-published_at').annotate(Count('comments'))\
+                                                        .prefetch_related('author')
     most_fresh_posts = list((fresh_posts)[:5])
 
     tags = Tag.objects.annotate(Count('posts'))
     most_popular_tags = list(tags.order_by('-posts__count')[:5])
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
