c0dad2f797bc73306a0ea3cf648196eb341567b2
Vladimir <v1ztep@gmail.com>
2021-04-13 17:39:47 +0300

    Подгрузил Теги заранее

--- blog/views.py
+++ blog/views.py
@@ -44,12 +44,12 @@ def serialize_tag(tag):
 
 def index(request):
     most_popular_posts = Post.objects.popular()[:5]\
-                                     .prefetch_related('author')\
+                                     .prefetch_related('author', 'tags')\
                                      .fetch_with_comments_count()
 
     most_fresh_posts = Post.objects.order_by('-published_at')[:5]\
                                    .annotate(Count('comments'))\
-                                   .prefetch_related('author')
+                                   .prefetch_related('author', 'tags')
 
     most_popular_tags = Tag.objects.popular()[:5]
 
