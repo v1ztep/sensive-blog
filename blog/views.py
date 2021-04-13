25053e181dfed743679b420525c38ccf0190d9c2
Vladimir <v1ztep@gmail.com>
2021-04-13 22:32:06 +0300

    annotate + Count под serialize_tag

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,9 @@ def serialize_post_optimized(post):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        "tags": [serialize_tag_optimized(tag) for tag in post.tags.annotate(
+            Count('posts')
+        )],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -39,6 +41,13 @@ def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
+    }
+
+
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts__count,
     }
 
 
@@ -56,7 +65,7 @@ def index(request):
     context = {
         'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
         'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
